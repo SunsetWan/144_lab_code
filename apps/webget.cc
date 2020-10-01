df64a5da0d90b237bddf215a55d181e26da10adb
sunset <sunsetwan@gmail.com>
2020-10-01 09:06:25 -0700

    lab0

---
+++ apps/webget.cc
@@ -0,0 +1,67 @@
+#include "socket.hh"
+#include "util.hh"
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+void get_URL(const string &host, const string &path) {
+    Address addr = Address(host, "http");
+    TCPSocket socket = TCPSocket();
+
+    // After this line of code is executed,
+    // the 3-way handshake is performed
+    // and a TCP connectin is established
+    // between the client and server.
+    socket.connect(addr); 
+
+    // Q: Why are there two consecutive "\r\n"?
+    // A: The end of the header section is indicated by 
+    //    an empty field line,
+    //    resulting in the transmission of two consecutive CR-LF pairs
+    // Ref link: https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
+    socket.write("GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\n\r\n");
+    
+    // Client side won't continue to write messages
+    socket.shutdown(SHUT_WR);
+
+    // Keep reading messages from Server side until meeting EOF
+    while (!socket.eof()) {
+        string res = socket.read();
+        cout << res;
+    }
+
+    socket.close();
+    return;
+
+}
+
+int main(int argc, char *argv[]) {
+    try {
+        if (argc <= 0) {
+            abort();  // For sticklers: don't try to access argv[0] if argc <= 0.
+        }
+
+        // The program takes two command-line arguments: the hostname and "path" part of the URL.
+        // Print the usage message unless there are these two arguments (plus the program name
+        // itself, so arg count = 3 in total).
+        if (argc != 3) {
+            cerr << "Usage: " << argv[0] << " HOST PATH\n";
+            cerr << "\tExample: " << argv[0] << " stanford.edu /class/cs144\n";
+            return EXIT_FAILURE;
+        }
+
+        // Get the command-line arguments.
+        const string host = argv[1];
+        const string path = argv[2];
+
+        // Call the student-written function.
+        get_URL(host, path);
+    } catch (const exception &e) {
+        cerr << e.what() << "\n";
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ libsponge/byte_stream.cc
@@ -0,0 +1,103 @@
+#include "byte_stream.hh"
+
+// Dummy implementation of a flow-controlled in-memory byte stream.
+
+// For Lab 0, please replace with a real implementation that passes the
+// automated checks run by `make check_lab0`.
+
+// You will need to add private members to the class declaration in `byte_stream.hh`
+
+template <typename... Targs>
+void DUMMY_CODE(Targs &&... /* unused */) {}
+
+using namespace std;
+
+// Required to use initialization list
+ByteStream::ByteStream(const size_t capacity): _capacity(capacity) {}
+
+size_t ByteStream::write(const string &data) {
+    size_t len = data.length();
+    if (len > _capacity - _buffer.size()) {
+        len = _capacity - _buffer.size();
+    }
+
+    _writeCount += len;
+
+    for (size_t i = 0; i < len; i++) {
+        _buffer.push_back(data[i]);
+    }
+    
+    return len;
+}
+
+//! \param[in] len bytes will be copied from the output side of the buffer
+string ByteStream::peek_output(const size_t len) const {
+    size_t length = len;
+
+    if (_buffer.empty()) return "";
+
+    if (length > _buffer.size()) {
+        length = _buffer.size();
+    }
+
+    return string().assign(_buffer.begin(), _buffer.begin() + length);
+}
+
+//! \param[in] len bytes will be removed from the output side of the buffer
+void ByteStream::pop_output(const size_t len) { 
+    size_t length = len;
+    if (_buffer.empty()) { return; }
+
+    if (length > _buffer.size()) {
+        length = _buffer.size();
+    }
+
+    _readCount += length;
+
+    for (size_t i = 0; i < length; i++) {
+        _buffer.pop_front();
+    }
+
+    return;
+
+}
+
+//! Read (i.e., copy and then pop) the next "len" bytes of the stream
+//! \param[in] len bytes will be popped and returned
+//! \returns a string
+std::string ByteStream::read(const size_t len) {
+    string res = peek_output(len);
+    pop_output(len);
+    return res;
+}
+
+void ByteStream::end_input() {
+    _isInputEnded = true;
+}
+
+bool ByteStream::input_ended() const { 
+    return _isInputEnded; 
+}
+
+size_t ByteStream::buffer_size() const { 
+    return _buffer.size(); 
+}
+
+bool ByteStream::buffer_empty() const { 
+    return _buffer.empty(); 
+}
+
+bool ByteStream::eof() const { 
+    return input_ended() && buffer_empty(); 
+}
+
+size_t ByteStream::bytes_written() const { 
+    return _writeCount; }
+
+size_t ByteStream::bytes_read() const { 
+    return _readCount; 
+}
+
+size_t ByteStream::remaining_capacity() const { 
+    return _capacity - _buffer.size(); 
+}
