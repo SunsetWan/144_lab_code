7a4ea80681b8e4af27c4d406943af971c6957f05
sunset <sunsetwan@gmail.com>
2020-11-17 21:42:47 -0800

    Add proper comments

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,4 +1,5 @@
 #include "wrapping_integers.hh"
+#include <iostream>
 
 // Dummy implementation of a 32-bit wrapping integer
 
@@ -43,6 +44,8 @@ uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
     uint32_t offset = n.raw_value() - isn.raw_value();
     // if checkpoint < 2^32 - 1, t = offset;
     // The low 32 bits of absolute seqno equals the low 32 bits of seqno.
+    // checkpoint's low 32 bits == offset
+    // offset is the low 32 bits
     uint64_t t = (checkpoint & 0xFFFFFFFF00000000) + offset;
     uint64_t result = t;
     if (abs(int64_t(t + (1ul << 32) - checkpoint)) < abs(int64_t(t - checkpoint))) {
@@ -53,15 +56,4 @@ uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
     }
     return result;
 
-    // uint64_t result = 0;
-    // uint64_t twoToThe32Power = (static_cast<uint64_t>(0x00000000FFFFFFFF) + 1);
-    // uint64_t divisor = checkpoint / twoToThe32Power;
-    // result = divisor * twoToThe32Power + static_cast<uint64_t>(offset);
-    // uint64_t diff1 = result > checkpoint ? result - checkpoint : checkpoint - result;
-    // uint64_t diff2 = result + twoToThe32Power - checkpoint;
-    // if (diff1 < diff2) {
-    //     return result;
-    // } else {
-    //     return result + twoToThe32Power;
-    // }
 }
