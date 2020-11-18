1c95ae2405f0f13348b786de2fef25daab5524c8
sunset <sunsetwan@gmail.com>
2020-11-18 07:22:05 -0800

    Be prepared to debug.

--- tests/send_transmit.cc
+++ tests/send_transmit.cc
@@ -15,87 +15,87 @@ int main() {
     try {
         auto rd = get_random_generator();
 
-        {
-            TCPConfig cfg;
-            WrappingInt32 isn(rd());
-            cfg.fixed_isn = isn;
+        // {
+        //     TCPConfig cfg;
+        //     WrappingInt32 isn(rd());
+        //     cfg.fixed_isn = isn;
 
-            TCPSenderTestHarness test{"Three short writes", cfg};
-            test.execute(ExpectSegment{}.with_no_flags().with_syn(true).with_payload_size(0).with_seqno(isn));
-            test.execute(AckReceived{WrappingInt32{isn + 1}});
-            test.execute(ExpectState{TCPSenderStateSummary::SYN_ACKED});
-            test.execute(WriteBytes{"ab"});
-            test.execute(ExpectSegment{}.with_data("ab").with_seqno(isn + 1));
-            test.execute(WriteBytes{"cd"});
-            test.execute(ExpectSegment{}.with_data("cd").with_seqno(isn + 3));
-            test.execute(WriteBytes{"abcd"});
-            test.execute(ExpectSegment{}.with_data("abcd").with_seqno(isn + 5));
-            test.execute(ExpectSeqno{WrappingInt32{isn + 9}});
-            test.execute(ExpectBytesInFlight{8});
-            test.execute(ExpectState{TCPSenderStateSummary::SYN_ACKED});
-        }
+        //     TCPSenderTestHarness test{"Three short writes", cfg};
+        //     test.execute(ExpectSegment{}.with_no_flags().with_syn(true).with_payload_size(0).with_seqno(isn));
+        //     test.execute(AckReceived{WrappingInt32{isn + 1}});
+        //     test.execute(ExpectState{TCPSenderStateSummary::SYN_ACKED});
+        //     test.execute(WriteBytes{"ab"});
+        //     test.execute(ExpectSegment{}.with_data("ab").with_seqno(isn + 1));
+        //     test.execute(WriteBytes{"cd"});
+        //     test.execute(ExpectSegment{}.with_data("cd").with_seqno(isn + 3));
+        //     test.execute(WriteBytes{"abcd"});
+        //     test.execute(ExpectSegment{}.with_data("abcd").with_seqno(isn + 5));
+        //     test.execute(ExpectSeqno{WrappingInt32{isn + 9}});
+        //     test.execute(ExpectBytesInFlight{8});
+        //     test.execute(ExpectState{TCPSenderStateSummary::SYN_ACKED});
+        // }
 
-        {
-            TCPConfig cfg;
-            WrappingInt32 isn(rd());
-            cfg.fixed_isn = isn;
+        // {
+        //     TCPConfig cfg;
+        //     WrappingInt32 isn(rd());
+        //     cfg.fixed_isn = isn;
 
-            TCPSenderTestHarness test{"Many short writes, continuous acks", cfg};
-            test.execute(ExpectSegment{}.with_no_flags().with_syn(true).with_payload_size(0).with_seqno(isn));
-            test.execute(AckReceived{WrappingInt32{isn + 1}});
-            test.execute(ExpectState{TCPSenderStateSummary::SYN_ACKED});
-            uint32_t max_block_size = 10;
-            uint32_t n_rounds = 10000;
-            size_t bytes_sent = 0;
-            for (uint32_t i = 0; i < n_rounds; ++i) {
-                string data;
-                uint32_t block_size = uniform_int_distribution<uint32_t>{1, max_block_size}(rd);
-                for (uint8_t j = 0; j < block_size; ++j) {
-                    uint8_t c = 'a' + ((i + j) % 26);
-                    data.push_back(c);
-                }
-                test.execute(ExpectSeqno{WrappingInt32{isn + uint32_t(bytes_sent) + 1}});
-                test.execute(WriteBytes(string(data)));
-                bytes_sent += block_size;
-                test.execute(ExpectBytesInFlight{block_size});
-                test.execute(
-                    ExpectSegment{}.with_seqno(isn + 1 + uint32_t(bytes_sent - block_size)).with_data(move(data)));
-                test.execute(ExpectNoSegment{});
-                test.execute(AckReceived{WrappingInt32{isn + 1 + uint32_t(bytes_sent)}});
-            }
-        }
+        //     TCPSenderTestHarness test{"Many short writes, continuous acks", cfg};
+        //     test.execute(ExpectSegment{}.with_no_flags().with_syn(true).with_payload_size(0).with_seqno(isn));
+        //     test.execute(AckReceived{WrappingInt32{isn + 1}});
+        //     test.execute(ExpectState{TCPSenderStateSummary::SYN_ACKED});
+        //     uint32_t max_block_size = 10;
+        //     uint32_t n_rounds = 10000;
+        //     size_t bytes_sent = 0;
+        //     for (uint32_t i = 0; i < n_rounds; ++i) {
+        //         string data;
+        //         uint32_t block_size = uniform_int_distribution<uint32_t>{1, max_block_size}(rd);
+        //         for (uint8_t j = 0; j < block_size; ++j) {
+        //             uint8_t c = 'a' + ((i + j) % 26);
+        //             data.push_back(c);
+        //         }
+        //         test.execute(ExpectSeqno{WrappingInt32{isn + uint32_t(bytes_sent) + 1}});
+        //         test.execute(WriteBytes(string(data)));
+        //         bytes_sent += block_size;
+        //         test.execute(ExpectBytesInFlight{block_size});
+        //         test.execute(
+        //             ExpectSegment{}.with_seqno(isn + 1 + uint32_t(bytes_sent - block_size)).with_data(move(data)));
+        //         test.execute(ExpectNoSegment{});
+        //         test.execute(AckReceived{WrappingInt32{isn + 1 + uint32_t(bytes_sent)}});
+        //     }
+        // }
 
-        {
-            TCPConfig cfg;
-            WrappingInt32 isn(rd());
-            cfg.fixed_isn = isn;
+        // {
+        //     TCPConfig cfg;
+        //     WrappingInt32 isn(rd());
+        //     cfg.fixed_isn = isn;
 
-            TCPSenderTestHarness test{"Many short writes, ack at end", cfg};
-            test.execute(ExpectSegment{}.with_no_flags().with_syn(true).with_payload_size(0).with_seqno(isn));
-            test.execute(AckReceived{WrappingInt32{isn + 1}}.with_win(65000));
-            test.execute(ExpectState{TCPSenderStateSummary::SYN_ACKED});
-            uint32_t max_block_size = 10;
-            uint32_t n_rounds = 1000;
-            size_t bytes_sent = 0;
-            for (uint32_t i = 0; i < n_rounds; ++i) {
-                string data;
-                uint32_t block_size = uniform_int_distribution<uint32_t>{1, max_block_size}(rd);
-                for (uint8_t j = 0; j < block_size; ++j) {
-                    uint8_t c = 'a' + ((i + j) % 26);
-                    data.push_back(c);
-                }
-                test.execute(ExpectSeqno{WrappingInt32{isn + uint32_t(bytes_sent) + 1}});
-                test.execute(WriteBytes(string(data)));
-                bytes_sent += block_size;
-                test.execute(ExpectBytesInFlight{bytes_sent});
-                test.execute(
-                    ExpectSegment{}.with_seqno(isn + 1 + uint32_t(bytes_sent - block_size)).with_data(move(data)));
-                test.execute(ExpectNoSegment{});
-            }
-            test.execute(ExpectBytesInFlight{bytes_sent});
-            test.execute(AckReceived{WrappingInt32{isn + 1 + uint32_t(bytes_sent)}});
-            test.execute(ExpectBytesInFlight{0});
-        }
+        //     TCPSenderTestHarness test{"Many short writes, ack at end", cfg};
+        //     test.execute(ExpectSegment{}.with_no_flags().with_syn(true).with_payload_size(0).with_seqno(isn));
+        //     test.execute(AckReceived{WrappingInt32{isn + 1}}.with_win(65000));
+        //     test.execute(ExpectState{TCPSenderStateSummary::SYN_ACKED});
+        //     uint32_t max_block_size = 10;
+        //     uint32_t n_rounds = 1000;
+        //     size_t bytes_sent = 0;
+        //     for (uint32_t i = 0; i < n_rounds; ++i) {
+        //         string data;
+        //         uint32_t block_size = uniform_int_distribution<uint32_t>{1, max_block_size}(rd);
+        //         for (uint8_t j = 0; j < block_size; ++j) {
+        //             uint8_t c = 'a' + ((i + j) % 26);
+        //             data.push_back(c);
+        //         }
+        //         test.execute(ExpectSeqno{WrappingInt32{isn + uint32_t(bytes_sent) + 1}});
+        //         test.execute(WriteBytes(string(data)));
+        //         bytes_sent += block_size;
+        //         test.execute(ExpectBytesInFlight{bytes_sent});
+        //         test.execute(
+        //             ExpectSegment{}.with_seqno(isn + 1 + uint32_t(bytes_sent - block_size)).with_data(move(data)));
+        //         test.execute(ExpectNoSegment{});
+        //     }
+        //     test.execute(ExpectBytesInFlight{bytes_sent});
+        //     test.execute(AckReceived{WrappingInt32{isn + 1 + uint32_t(bytes_sent)}});
+        //     test.execute(ExpectBytesInFlight{0});
+        // }
 
         {
             TCPConfig cfg;
@@ -126,26 +126,26 @@ int main() {
             test.execute(ExpectNoSegment{});
         }
 
-        {
-            TCPConfig cfg;
-            WrappingInt32 isn(rd());
-            cfg.fixed_isn = isn;
+        // {
+        //     TCPConfig cfg;
+        //     WrappingInt32 isn(rd());
+        //     cfg.fixed_isn = isn;
 
-            TCPSenderTestHarness test{"Immediate writes respect the window", cfg};
-            test.execute(ExpectSegment{}.with_no_flags().with_syn(true).with_payload_size(0).with_seqno(isn));
-            test.execute(AckReceived{WrappingInt32{isn + 1}}.with_win(3));
-            test.execute(ExpectState{TCPSenderStateSummary::SYN_ACKED});
-            test.execute(WriteBytes("01"));
-            test.execute(ExpectBytesInFlight{2});
-            test.execute(ExpectSegment{}.with_data("01"));
-            test.execute(ExpectNoSegment{});
-            test.execute(ExpectSeqno{WrappingInt32{isn + 1 + 2}});
-            test.execute(WriteBytes("23"));
-            test.execute(ExpectBytesInFlight{3});
-            test.execute(ExpectSegment{}.with_data("2"));
-            test.execute(ExpectNoSegment{});
-            test.execute(ExpectSeqno{WrappingInt32{isn + 1 + 3}});
-        }
+        //     TCPSenderTestHarness test{"Immediate writes respect the window", cfg};
+        //     test.execute(ExpectSegment{}.with_no_flags().with_syn(true).with_payload_size(0).with_seqno(isn));
+        //     test.execute(AckReceived{WrappingInt32{isn + 1}}.with_win(3));
+        //     test.execute(ExpectState{TCPSenderStateSummary::SYN_ACKED});
+        //     test.execute(WriteBytes("01"));
+        //     test.execute(ExpectBytesInFlight{2});
+        //     test.execute(ExpectSegment{}.with_data("01"));
+        //     test.execute(ExpectNoSegment{});
+        //     test.execute(ExpectSeqno{WrappingInt32{isn + 1 + 2}});
+        //     test.execute(WriteBytes("23"));
+        //     test.execute(ExpectBytesInFlight{3});
+        //     test.execute(ExpectSegment{}.with_data("2"));
+        //     test.execute(ExpectNoSegment{});
+        //     test.execute(ExpectSeqno{WrappingInt32{isn + 1 + 3}});
+        // }
 
     } catch (const exception &e) {
         cerr << e.what() << endl;
