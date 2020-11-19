bc13616ba0364ddd960990c03930817c7ba37475
sunset <sunsetwan@gmail.com>
2020-11-19 04:35:47 -0800

    Fix another bug of lab3; Reason: forget to initialize _retransmissionTimeout!

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -20,7 +20,8 @@ using namespace std;
 TCPSender::TCPSender(const size_t capacity, const uint16_t retx_timeout, const std::optional<WrappingInt32> fixed_isn)
     : _isn(fixed_isn.value_or(WrappingInt32{random_device()()}))
     , _initial_retransmission_timeout{retx_timeout}
-    , _stream(capacity) {}
+    , _stream(capacity)
+    , _retransmissionTimeout(retx_timeout) {}
 
 uint64_t TCPSender::bytes_in_flight() const {
     return _bytesInFlight;
